6dd1e932da3f9d0742468d0805cc61abea77af44
Nicole Kraemer <kraemer@ma.tum.de>
2013-02-06 00:00:00 +0000

    version 0.2-5

--- R/pcr.R
+++ R/pcr.R
@@ -1,34 +1,40 @@
-pcr<-function(X,y,scale=TRUE,eps=0.000001){
+pcr<-function (X, y, scale = TRUE,m=min(ncol(X),nrow(X)-1),eps = 1e-06)
+{
     p <- ncol(X)
     n <- nrow(X)
-    m<-p
-    Beta <- matrix(, p, m) # matrix of regression coefficients
+    Beta <- matrix(, p, m)
     mean.y <- mean(y)
+    #m=min(n,p)
     y <- scale(y, scale = FALSE)
     mean.X <- apply(X, 2, mean)
-    if (scale==FALSE){
-        sd.X<-rep(1,p)
+    if (scale == FALSE) {
+        sd.X <- rep(1, p)
     }
-    if (scale==TRUE){
-    sd.X <- apply(X, 2, sd)
-    sd.X[sd.X == 0] = 1
+    if (scale == TRUE) {
+        sd.X <- apply(X, 2, sd)
+        sd.X[sd.X == 0] = 1
     }
     X <- X - rep(1, nrow(X)) %*% t(mean.X)
     X <- X/(rep(1, nrow(X)) %*% t(sd.X))
-    S<-t(X)%*%X
-    b<-t(X)%*%y
-    eig<-eigen(S)
-    lambda<-eig$values
-    U<-eig$vectors
-    lambdainv<-1/lambda
-    lambdainv[lambda<eps]=0
-    #cat(paste("lambdainv is ",lambdainv,"\n"))
-    dummy<-diag(lambdainv)%*%t(U)%*%b
-    for (i in 1:m){
-        Beta[,i]<-U[,1:i,drop=FALSE]%*%dummy[1:i]
-    }
+    b <- t(X) %*% y
+    	my.svd=svd(X)
+    	lambda <- (my.svd$d)^2
+    	U <- my.svd$v
+    	lambdainv <- 1/lambda
+    	lambdainv[lambda < eps] = 0
+    	dummy <- diag(lambdainv) %*% t(U) %*% b
+    	for (i in 1:m) {
+		#cat(paste("component number ",i,"\n"))
+        	Beta[, i] <- U[, 1:i, drop = FALSE] %*% dummy[1:i]
+    	}
+    	#if (p>m){
+        #	for (i in ((m+1):(p))){
+        #    		Beta[,i]=Beta[,m]
+        #	}
+    	#}
+    
     coefficients <- matrix(0, p, m + 1)
     coefficients[, 2:(m + 1)] = Beta/(sd.X %*% t(rep(1, m)))
     intercept <- rep(mean.y, m + 1) - t(coefficients) %*% mean.X
-    return(list(intercept=intercept,coefficients=coefficients))
+    return(list(intercept = intercept, coefficients = coefficients))
 }
--- R/pcr.cv.R
+++ R/pcr.cv.R
@@ -1,4 +1,4 @@
-pcr.cv<-function(X,y,k=10,groups=NULL,scale=TRUE,eps=0.000001,plot.it=FALSE){
+pcr.cv<-function(X,y,k=10,m=min(ncol(X),nrow(X)-1),groups=NULL,scale=TRUE,eps=0.000001,plot.it=FALSE,compute.jackknife=TRUE){
       n<-nrow(X)    
       p<-ncol(X)
    if (is.null(groups)==FALSE){
@@ -12,20 +12,31 @@ pcr.cv<-function(X,y,k=10,groups=NULL,scale=TRUE,eps=0.000001,plot.it=FALSE){
     }
     
     all.folds <- split(sample(1:n),f)
-    cv.error.matrix = matrix(0, k,p+1)
+    ntrain = vector(length = k)
+    for (i in 1:k) {
+        ntrain[i] = n - length(all.folds[[i]])
+    }
+    ntrain.min = min(ntrain)
+    m = min(m, ntrain.min - 1)
+    cv.error.matrix = matrix(0, k,m+1)
     rownames(cv.error.matrix)=my.names
-    colnames(cv.error.matrix)=0:p
-    coefficients.jackknife<-array(dim=c(p,p+1,k))
+    colnames(cv.error.matrix)=0:m
+    coefficients.jackknife=NULL
+    if (compute.jackknife==TRUE){
+    coefficients.jackknife<-array(dim=c(p,m+1,k))
+    }
     for (i in seq(k)) {
         omit <- all.folds[[i]]
         Xtrain = X[-omit, , drop = FALSE]
         ytrain = y[-omit]
         Xtest = X[omit, , drop = FALSE]
         ytest = y[omit]
-        pcr.object <- pcr(Xtrain,ytrain,scale,eps)
-        res <- matrix(, length(ytest),p+1)
+        pcr.object <- pcr(Xtrain,ytrain,scale,m,eps)
+        res <- matrix(, length(ytest),m+1)
+        if (compute.jackknife==TRUE){
         coefficients.jackknife[,,i]=pcr.object$coefficients
-        for (j in 1:(p+1)){
+	}
+        for (j in 1:(m+1)){
             res[,j]<-ytest - pcr.object$intercept[j] - Xtest%*%pcr.object$coefficients[,j]
         
         }
@@ -34,9 +45,9 @@ pcr.cv<-function(X,y,k=10,groups=NULL,scale=TRUE,eps=0.000001,plot.it=FALSE){
     cv.error <- apply(cv.error.matrix,2,mean)
     m.opt <- which.min(cv.error)-1
     if (plot.it == TRUE) {
-        plot(0:p, cv.error, type = "l")
+        plot(0:m, cv.error, type = "l")
     }
-    pcr.object <- pcr(X,y,eps=eps)
+    pcr.object <- pcr(X,y,scale,m.opt,eps=eps)
     coefficients <- pcr.object$coefficients[,m.opt+1]
     intercept <- pcr.object$intercept[m.opt+1]
     return(list(intercept = intercept, coefficients = coefficients, 
